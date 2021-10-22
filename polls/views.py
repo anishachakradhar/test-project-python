8d43439fda47424cfb93a18e2f7ae9b1efc75dff
anishachakradhar <anishachakradhar@gmail.com>
2021-10-23 01:36:09 +0545

    Add templates

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,5 @@
+from .models import Question
+from django.template import loader
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,7 +7,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello world.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
