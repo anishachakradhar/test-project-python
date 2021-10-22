fc47185ec5f82b071b7444485c0ee16f051a734e
anishachakradhar <anishachakradhar@gmail.com>
2021-10-23 02:01:56 +0545

    Add render, context, 404, url name, app name

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,30 @@
 from .models import Question
-from django.template import loader
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
 # Create your views here.
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+
+    question = get_object_or_404(Question, pk=question_id)
+
+    context = {
+        'question': question
+    }
+
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
