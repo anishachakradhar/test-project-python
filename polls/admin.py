1e9bfdd5f9fc8bd73077c29cb2ef42e916f8a599
anishachakradhar <anishachakradhar@gmail.com>
2021-10-25 22:08:49 +0545

    Remove commented codes

--- polls/admin.py
+++ polls/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
-# admin.site.register(Choice)
-
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
--- polls/models.py
+++ polls/models.py
@@ -11,9 +11,6 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-#    def __str__(self):
-#         return self.question_text
-
     @admin.display(
         boolean=True,
         ordering='pub_date',
